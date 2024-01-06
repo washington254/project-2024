5d195e808f3340ef71e3682f60f12b80435440ca
washington maina <103877612+washington254@users.noreply.github.com>
2024-01-06 10:24:59 +0000

    fix: Fix incorrect new z position calculation

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ export default function App() {
 
       // Adjust the camera position based on screen size
       const distanceModifier = innerWidth < 768 ? 3.1 : 0; // Adjust this condition based on your design
-      const newZPosition = 4.5 + distanceModifier;
+      const newZPosition = 2.5 + distanceModifier;
 
       // Update the camera position if the ref is defined
       if (cameraRef.current) {
