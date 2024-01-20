1f9aaee7686a28d23c104baea8c00164738be3e6
washington maina <103877612+washington254@users.noreply.github.com>
2024-01-20 07:47:27 +0000

    style: Remove unused variable 'x'

--- src/App.jsx
+++ src/App.jsx
@@ -30,8 +30,6 @@ export default function App() {
   const mapNumRange = (num, inMin, inMax, outMin, outMax) =>
     ((num - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
 
-  let x;
-
   useEffect(() => {
     const landingPage = skillsPageBackgroundRef.current;
     const newY =
