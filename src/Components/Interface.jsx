fb1ae7ea31305b44ad29bdeb733ed523787b13d0
washington maina <103877612+washington254@users.noreply.github.com>
2024-01-25 04:16:47 -0800

    Add files via upload

--- src/Components/Interface.jsx
+++ src/Components/Interface.jsx
@@ -95,10 +95,12 @@ export function Interface(props) {
   const { section, device } = props;
   return (
     <div className={`${styles.interface} interface`}>
+      <Navbar />
       <AboutSection />
       <Skills section={section} />
       <Courses />
       <Contact />
+      <Footer />
     </div>
   );
 }
@@ -108,13 +110,9 @@ const AboutSection = () => {
     <Section
       className={`${styles.interfaceSection}  interface-part interface-welcome`}
     >
+     
       <div className="ctas">
-        <div className="navbar">
-          <img src="./logo.svg" alt="logo" />
-          <a href="#" className="cta-btn">
-            Get in touch
-          </a>
-        </div>
+       
         <h1 className="main-text">
           Hi, Traders <br /> Welcome to Goodwill Education{" "}
         </h1>
@@ -148,7 +146,8 @@ const Courses = () => {
 const Contact = () => {
   return (
     <Section className="contact-container">
-      <h2 className="heading-contact">Contact me</h2>
+     <div className="contact-left">
+      <h2 className="heading-contact">Contact Us</h2>
       <div className="contact-form">
         <form>
           <label htmlFor="name" className="label">
@@ -172,6 +171,39 @@ const Contact = () => {
           <button className="submit-button">Submit</button>
         </form>
       </div>
+     </div>
+      <div className="contact-right">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, inventore.</p>
+      </div>
     </Section>
   );
 };
+
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+    <div className="logo">
+      <img src="./logo.svg" alt="logo" />
+    </div>
+    {/* <h1>LOGO</h1> */}
+    <a href="#" className="cta-btn">
+      Subscribe
+    </a>
+
+  </nav>
+  )
+}
+const Footer = () => {
+  return (
+    <footer>
+      <ul>
+        <li><a href="#">LINK ONE</a></li>
+        <li><a href="#">LINK TWO</a></li>
+        <li><a href="#">LINK THREE</a></li>
+        <li><a href="#">LINK FOUR</a></li>
+      </ul>
+    </footer>
+  )
+}
+
