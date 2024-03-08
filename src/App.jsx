e9dabfae665d2d2f0d46ea70003ba67fffd16993
washington maina <103877612+washington254@users.noreply.github.com>
2024-03-08 12:52:12 +0000

    dele

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import styles from "./app.module.css";
 import { Experience } from "./Components/Experience";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Interface } from "./Components/Interface";
 import { SimpleCameraRig } from "./Components/SimpleCameraRig";
@@ -29,10 +29,13 @@ export default function App() {
   const cameraRef = useRef();
 
   const isWideScreen = window.innerWidth > 2000;
-  const pages = isWideScreen ? (device === 'web' ? 4.6 : 5.1) : (device === 'web' ? 5.6 : 5.1);
-
-
-
+  const pages = isWideScreen
+    ? device === "web"
+      ? 4.6
+      : 5.1
+    : device === "web"
+      ? 5.6
+      : 5.1;
 
   const mapNumRange = (num, inMin, inMax, outMin, outMax) =>
     ((num - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
@@ -56,64 +59,68 @@ export default function App() {
 
   return (
     <main className={`${styles.main} ${device}`}>
-     <div className={styles.skillsPage} ref={skillsPageBackgroundRef}>
-        <video className="back-video" autoPlay muted loop style={{
-          width:device==='web'?'100%':'auto',
-          height:'100%',
-          // transform:`translateX(${backVideoX}px)`
-        }}>
+      <div className={styles.skillsPage} ref={skillsPageBackgroundRef}>
+        <video
+          className="back-video"
+          autoPlay
+          muted
+          loop
+          style={{
+            width: device === "web" ? "100%" : "auto",
+            height: "100%",
+            // transform:`translateX(${backVideoX}px)`
+          }}
+        >
           {/* <source src="/bACKGROUND.mp4" type="video/mp4"/> */}
           {/* stock-market-2023-11-27-05-36-31-utc_compressed */}
         </video>
-
       </div>
       <MotionConfig
-      transition={{
-        type: "spring",
-        mass: 5,
-        stiffness: 500,
-        damping: 50,
-        restDelta: 0.0001
-      }}
+        transition={{
+          type: "spring",
+          mass: 5,
+          stiffness: 500,
+          damping: 50,
+          restDelta: 0.0001,
+        }}
       >
-   <Canvas>
-        <PerspectiveCamera
-          makeDefault
-          position={[3.2, 2.7, 4.5]} // Set the position of the camera
-          fov={25} // Field of view
-          near={0.1}
-          far={1000}
-          
-          ref={cameraRef}
-        />
-         <HandleResize cameraRef={cameraRef} setDevice={setDevice}  />
+        <Canvas>
+          <PerspectiveCamera
+            makeDefault
+            position={[3.2, 2.7, 4.5]} // Set the position of the camera
+            fov={25} // Field of view
+            near={0.1}
+            far={1000}
+            ref={cameraRef}
+          />
+          <HandleResize cameraRef={cameraRef} setDevice={setDevice} />
 
-        {/* <OrbitControls enableZoom={false} /> */}
+          {/* <OrbitControls enableZoom={false} /> */}
 
-        <SimpleCameraRig></SimpleCameraRig>
+          <SimpleCameraRig></SimpleCameraRig>
 
           <ScrollControls pages={pages} damping={0.2} maxSpeed={0.7}>
-          <ScrollManager
-            section={section}
-            onSectionChange={setSection}
-            setScrollOffset={setScrollOffset}
-            setScrollData={setScrollData}
-          />
-          <Scroll>
-            <Experience
-              device={device}
+            <ScrollManager
               section={section}
-              cameraRef={cameraRef}
+              onSectionChange={setSection}
+              setScrollOffset={setScrollOffset}
+              setScrollData={setScrollData}
             />
-          </Scroll>
-          <Scroll html>
-            <Interface device={device} section={section} />
-          </Scroll>
-        </ScrollControls>
-      </Canvas>
+            <Scroll>
+              <Suspense fallback={"loading experience..."}>
+                <Experience
+                  device={device}
+                  section={section}
+                  cameraRef={cameraRef}
+                />
+              </Suspense>
+            </Scroll>
+            <Scroll html>
+              <Interface device={device} section={section} />
+            </Scroll>
+          </ScrollControls>
+        </Canvas>
       </MotionConfig>
-   
-
     </main>
   );
 }
